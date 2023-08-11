6c9793f5f890c2b265672be811a38e1b62bc1cb6
cr4zysecu@gmail.com <ec2-user@ip-10-0-10-132.ap-northeast-2.compute.internal>
2023-08-11 13:24:31 +0000

    tfc state

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-  // cloud {
-  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
-  //   hostname     = "app.terraform.io"      # default
+   cloud {
+     organization = "j2h0-org"         # 생성한 ORG 이름 지정
+     hostname     = "app.terraform.io"      # default
 
-  //   workspaces {
-  //     name = "collaboration"  # 없으면 생성됨
-  //   }
-  // }
+     workspaces {
+       name = "collaboration"  # 없으면 생성됨
+     }
+   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -19,7 +19,7 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      Project = "T101-Study" #Tom and Jerry
+      Project = "T101-Study" #Jerry
     }
   }
 }
